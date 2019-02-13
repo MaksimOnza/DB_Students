ffb32a16c10aa926ce016ec9d349364efcd11de3
MaksimOnza <Maximus83onza@gmail.com>
2019-02-13 14:41:18 +0200

    Update Comments

--- students/frame/StudentsFrame.java
+++ students/frame/StudentsFrame.java
@@ -39,7 +39,7 @@ import students.logic.ManagementSystem;
 import students.logic.Student;
 
 public class StudentsFrame extends JFrame implements ActionListener, ListSelectionListener, ChangeListener {
-    // Введем сразу имена для кнопок - потом будем их использовать в обработчиках
+    // РРјРµРЅР° РґР»СЏ РєРЅРѕРїРѕРє
 
     private static final String MOVE_GR = "moveGroup";
     private static final String CLEAR_GR = "clearGroup";
@@ -53,106 +53,77 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
     private JSpinner spYear;
 
     public StudentsFrame() throws Exception {
-        // Устанавливаем layout для всей клиентской части формы
+        // layout РґР»СЏ РІСЃРµР№ РєР»РёРµРЅС‚СЃРєРѕР№ С‡Р°СЃС‚Рё С„РѕСЂРјС‹
         getContentPane().setLayout(new BorderLayout());
 
-        // Создаем строку меню
         JMenuBar menuBar = new JMenuBar();
-        // Создаем выпадающее меню
-        JMenu menu = new JMenu("Отчеты");
-        // Создаем пункт в выпадающем меню
-        JMenuItem menuItem = new JMenuItem("Все студенты");
+        JMenu menu = new JMenu("РћС‚С‡РµС‚С‹");
+        JMenuItem menuItem = new JMenuItem("Р’СЃРµ СЃС‚СѓРґРµРЅС‚С‹");
         menuItem.setName(ALL_STUDENTS);
-        // Добавляем листенер
         menuItem.addActionListener(this);
-        // Вставляем пункт меню в выпадающее меню
         menu.add(menuItem);
-        // Вставляем выпадающее меню в строку меню
         menuBar.add(menu);
-        // Устанавливаем меню для формы
         setJMenuBar(menuBar);
 
-        // Создаем верхнюю панель, где будет поле для ввода года
+        // Р’РµСЂС…РЅСЏСЏ РїР°РЅРµР»СЊ
         JPanel top = new JPanel();
-        // Устанавливаем для нее layout
         top.setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        // Вставляем пояснительную надпись
-        top.add(new JLabel("Год обучения:"));
-        // Делаем спин-поле
-        // 1. Задаем модель поведения - только цифры
-        // 2. Вставляем в панель
+        top.add(new JLabel("Р“РѕРґ РѕР±СѓС‡РµРЅРёСЏ:"));
         SpinnerModel sm = new SpinnerNumberModel(2006, 1900, 2100, 1);
         spYear = new JSpinner(sm);
-        // Добавляем листенер
         spYear.addChangeListener(this);
         top.add(spYear);
 
-        // Создаем нижнюю панель и задаем ей layout
+        // РќРёР¶РЅСЏСЏ РїР°РЅРµР»СЊ
         JPanel bot = new JPanel();
         bot.setLayout(new BorderLayout());
 
-        // Создаем левую панель для вывода списка групп
-        // Она у нас
+        // Р›РµРІР°СЏ РїР°РЅРµР»СЊ
         GroupPanel left = new GroupPanel();
-        // Задаем layout и задаем "бордюр" вокруг панели
         left.setLayout(new BorderLayout());
         left.setBorder(new BevelBorder(BevelBorder.LOWERED));
 
-        // Получаем коннект к базе и создаем объект ManagementSystem
         ms = ManagementSystem.getInstance();
-        // Получаем список групп
         Vector<Group> gr = new Vector<Group>(ms.getGroups());
-        // Создаем надпись
-        left.add(new JLabel("Группы:"), BorderLayout.NORTH);
-        // Создаем визуальный список и вставляем его в скроллируемую
-        // панель, которую в свою очередь уже кладем на панель left
+        left.add(new JLabel("Р“СЂСѓРїРїС‹:"), BorderLayout.NORTH);
         grpList = new JList(gr);
-        // Добавляем листенер
         grpList.addListSelectionListener(this);
-        // Сразу выделяем первую группу
         grpList.setSelectedIndex(0);
         left.add(new JScrollPane(grpList), BorderLayout.CENTER);
-        // Создаем кнопки для групп
-        JButton btnMvGr = new JButton("Переместить");
+        JButton btnMvGr = new JButton("РџРµСЂРµРјРµСЃС‚РёС‚СЊ");
         btnMvGr.setName(MOVE_GR);
-        JButton btnClGr = new JButton("Очистить");
+        JButton btnClGr = new JButton("РћС‡РёСЃС‚РёС‚СЊ");
         btnClGr.setName(CLEAR_GR);
-        // Добавляем листенер
         btnMvGr.addActionListener(this);
         btnClGr.addActionListener(this);
-        // Создаем панель, на которую положим наши кнопки и кладем ее вниз
+        // РџР°РЅРµР»СЊ РґР»СЏ РєРЅРѕРїРѕРє
         JPanel pnlBtnGr = new JPanel();
         pnlBtnGr.setLayout(new GridLayout(1, 2));
         pnlBtnGr.add(btnMvGr);
         pnlBtnGr.add(btnClGr);
         left.add(pnlBtnGr, BorderLayout.SOUTH);
 
-        // Создаем правую панель для вывода списка студентов
+        // РџСЂР°РІР°СЏ РїР°РЅРµР»СЊ
         JPanel right = new JPanel();
-        // Задаем layout и задаем "бордюр" вокруг панели
         right.setLayout(new BorderLayout());
         right.setBorder(new BevelBorder(BevelBorder.LOWERED));
 
-        // Создаем надпись
-        right.add(new JLabel("Студенты:"), BorderLayout.NORTH);
-        // Создаем таблицу и вставляем ее в скроллируемую
-        // панель, которую в свою очередь уже кладем на панель right
-        // Наша таблица пока ничего не умеет - просто положим ее как заготовку
-        // Сделаем в ней 4 колонки - Фамилия, Имя, Отчество, Дата рождения
+        right.add(new JLabel("Г‘ГІГіГ¤ГҐГ­ГІГ»:"), BorderLayout.NORTH);
+        // РЎРѕР·РґР°РµРј С‚Р°Р±Р»РёС†Сѓ
         stdList = new JTable(1, 4);
         right.add(new JScrollPane(stdList), BorderLayout.CENTER);
-        // Создаем кнопки для студентов
-        JButton btnAddSt = new JButton("Добавить");
+        // РљРЅРѕРїРєРё РґР»СЏ СЃС‚СѓРґРµРЅС‚РѕРІ
+        JButton btnAddSt = new JButton("Р”РѕР±Р°РІРёС‚СЊ");
         btnAddSt.setName(INSERT_ST);
         btnAddSt.addActionListener(this);
-        JButton btnUpdSt = new JButton("Исправить");
+        JButton btnUpdSt = new JButton("РСЃРїСЂР°РІРёС‚СЊ");
         btnUpdSt.setName(UPDATE_ST);
         btnUpdSt.addActionListener(this);
-        JButton btnDelSt = new JButton("Удалить");
+        JButton btnDelSt = new JButton("РЈРґР°Р»РёС‚СЊ");
         btnDelSt.setName(DELETE_ST);
         btnDelSt.addActionListener(this);
-        // Создаем панель, на которую положим наши кнопки и кладем ее вниз
+        // РџР°РЅРµР»СЊ РґР»СЏ РєРЅРѕРїРѕРє
         JPanel pnlBtnSt = new JPanel();
         pnlBtnSt.setLayout(new GridLayout(1, 3));
         pnlBtnSt.add(btnAddSt);
@@ -160,19 +131,17 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
         pnlBtnSt.add(btnDelSt);
         right.add(pnlBtnSt, BorderLayout.SOUTH);
 
-        // Вставляем панели со списками групп и студентов в нижнюю панель
         bot.add(left, BorderLayout.WEST);
         bot.add(right, BorderLayout.CENTER);
 
-        // Вставляем верхнюю и нижнюю панели в форму
         getContentPane().add(top, BorderLayout.NORTH);
         getContentPane().add(bot, BorderLayout.CENTER);
 
-        // Задаем границы формы
+        // РіСЂР°РЅРёС†С‹ С„РѕСЂРјС‹
         setBounds(100, 100, 700, 500);
     }
 
-    // Метод для обеспечения интерфейса ActionListener
+    // РњРµС‚РѕРґ РґР»СЏ РѕР±РµСЃРїРµС‡РµРЅРёСЏ РёРЅС‚РµСЂС„РµР№СЃР° ActionListener
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() instanceof Component) {
             Component c = (Component) e.getSource();
@@ -197,70 +166,54 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
         }
     }
 
-    // Метод для обеспечения интерфейса ListSelectionListener
+    // РњРµС‚РѕРґ РґР»СЏ РѕР±РµСЃРїРµС‡РµРЅРёСЏ РёРЅС‚РµСЂС„РµР№СЃР° ListSelectionListener
     public void valueChanged(ListSelectionEvent e) {
         if (!e.getValueIsAdjusting()) {
             reloadStudents();
         }
     }
 
-    // Метод для обеспечения интерфейса ChangeListener---зависимость от года
+    // РњРµС‚РѕРґ РґР»СЏ РѕР±РµСЃРїРµС‡РµРЅРёСЏ РёРЅС‚РµСЂС„РµР№СЃР° ChangeListener
     public void stateChanged(ChangeEvent e) {
         reloadStudents();
     }
 
-    // метод для обновления списка студентов для определенной группы
+    // РѕР±РЅРѕРІР»РµРЅРёРµ СЃРїРёСЃРєР° СЃС‚СѓРґРµРЅС‚РѕРІ РґР»СЏ РѕРїСЂРµРґРµР»РµРЅРЅРѕР№ РіСЂСѓРїРїС‹
     public void reloadStudents() {
-        // Создаем анонимный класс для потока
+	    // РЎРѕР·РґР°РµРј Р°РЅРѕРЅРёРјРЅС‹Р№ РєР»Р°СЃСЃ РґР»СЏ РїРѕС‚РѕРєР°
         Thread t = new Thread() {
-            // Переопределяем в нем метод run
 
             public void run() {
                 if (stdList != null) {
-                    // Получаем выделенную группу
                     Group g = (Group) grpList.getSelectedValue();
-                    // Получаем число из спинера
                     int y = ((SpinnerNumberModel) spYear.getModel()).getNumber().intValue();
                     try {
-                        // Получаем список студентов
                         Collection<Student> s = ms.getStudentsFromGroup(g, y);
-                        // И устанавливаем модель для таблицы с новыми данными
                         stdList.setModel(new StudentTableModel(new Vector<Student>(s)));
                     } catch (SQLException e) {
                         JOptionPane.showMessageDialog(StudentsFrame.this, e.getMessage());
                     }
                 }
             }
-            // Окончание нашего метода run
         };
-        // Окончание определения анонимного класса
-
-        // И теперь мы запускаем наш поток
         t.start();
     }
 
-    // метод для переноса группы
+    // РјРµС‚РѕРґ РґР»СЏ РїРµСЂРµРЅРѕСЃР° РіСЂСѓРїРїС‹
     private void moveGroup() {
         Thread t = new Thread() {
 
             public void run() {
-                // Если группа не выделена - выходим. Хотя это крайне маловероятно
                 if (grpList.getSelectedValue() == null) {
                     return;
                 }
                 try {
-                    // Получаем выделенную группу
                     Group g = (Group) grpList.getSelectedValue();
-                    // Получаем число из спинера
                     int y = ((SpinnerNumberModel) spYear.getModel()).getNumber().intValue();
-                    // Создаем наш диалог
+                    // Г‘Г®Г§Г¤Г ГҐГ¬ Г­Г Гё Г¤ГЁГ Г«Г®ГЈ
                     GroupDialog gd = new GroupDialog(y, ms.getGroups());
-                    // Задаем ему режим модальности - нельзя ничего кроме него выделить
                     gd.setModal(true);
-                    // Показываем диалог
                     gd.setVisible(true);
-                    // Если нажали кнопку OK - перемещаем в новую группу с новым годом
-                    // и перегружаем список студентов
                     if (gd.getResult()) {
                         ms.moveStudentsToGroup(g, y, gd.getGroup(), gd.getYear());
                         reloadStudents();
@@ -273,24 +226,18 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
         t.start();
     }
 
-    // метод для очистки группы
+    // РјРµС‚РѕРґ РґР»СЏ РѕС‡РёСЃС‚РєРё РіСЂСѓРїРїС‹
     private void clearGroup() {
         Thread t = new Thread() {
 
             public void run() {
-                // Проверяем - выделена ли группа
                 if (grpList.getSelectedValue() != null) {
                     if (JOptionPane.showConfirmDialog(StudentsFrame.this,
-                            "Вы хотите удалить студентов из группы?", "Удаление студентов",
                             JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-                        // Получаем выделенную группу
                         Group g = (Group) grpList.getSelectedValue();
-                        // Получаем число из спинера
                         int y = ((SpinnerNumberModel) spYear.getModel()).getNumber().intValue();
                         try {
-                            // Удалили студентов из группы
                             ms.removeStudentsFromGroup(g, y);
-                            // перегрузили список студентов
                             reloadStudents();
                         } catch (SQLException e) {
                             JOptionPane.showMessageDialog(StudentsFrame.this, e.getMessage());
@@ -302,15 +249,12 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
         t.start();
     }
 
-    // метод для добавления студента
+    // РјРµС‚РѕРґ РґР»СЏ РґРѕР±Р°РІР»РµРЅРёСЏ СЃС‚СѓРґРµРЅС‚Р°
     private void insertStudent() {
         Thread t = new Thread() {
 
             public void run() {
                 try {
-                    // Добавляем нового студента - поэтому true
-                    // Также заметим, что необходимо указать не просто this, а StudentsFrame.this
-                    // Иначе класс не будет воспринят - он же другой - анонимный
                     StudentDialog sd = new StudentDialog(ms.getGroups(), true, StudentsFrame.this);
                     sd.setModal(true);
                     sd.setVisible(true);
@@ -327,21 +271,16 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
         t.start();
     }
 
-    // метод для редактирования студента
+    // РјРµС‚РѕРґ РґР»СЏ СЂРµРґР°РєС‚РёСЂРѕРІР°РЅРёСЏ СЃС‚СѓРґРµРЅС‚Р°
     private void updateStudent() {
         Thread t = new Thread() {
 
             public void run() {
                 if (stdList != null) {
                     StudentTableModel stm = (StudentTableModel) stdList.getModel();
-                    // Проверяем - выделен ли хоть какой-нибудь студент
                     if (stdList.getSelectedRow() >= 0) {
-                        // Вот где нам пригодился метод getStudent(int rowIndex)
                         Student s = stm.getStudent(stdList.getSelectedRow());
                         try {
-                            // Исправляем данные на студента - поэтому false
-                            // Также заметим, что необходимо указать не просто this, а StudentsFrame.this
-                            // Иначе класс не будет воспринят - он же другой - анонимный
                             StudentDialog sd = new StudentDialog(ms.getGroups(), false, StudentsFrame.this);
                             sd.setStudent(s);
                             sd.setModal(true);
@@ -354,10 +293,10 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
                         } catch (SQLException e) {
                             JOptionPane.showMessageDialog(StudentsFrame.this, e.getMessage());
                         }
-                    } // Если студент не выделен - сообщаем пользователю, что это необходимо
+                    } 
                     else {
                         JOptionPane.showMessageDialog(StudentsFrame.this,
-                                "Необходимо выделить студента в списке");
+                                "РќРµРѕР±С…РѕРґРёРјРѕ РІС‹РґРµР»РёС‚СЊ СЃС‚СѓРґРµРЅС‚Р° РІ СЃРїРёСЃРєРµ");
                     }
                 }
             }
@@ -365,19 +304,17 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
         t.start();
     }
 
-    // метод для удаления студента
+    // РјРµС‚РѕРґ РґР»СЏ СѓРґР°Р»РµРЅРёСЏ СЃС‚СѓРґРµРЅС‚Р°
     private void deleteStudent() {
         Thread t = new Thread() {
 
             public void run() {
                 if (stdList != null) {
                     StudentTableModel stm = (StudentTableModel) stdList.getModel();
-                    // Проверяем - выделен ли хоть какой-нибудь студент
                     if (stdList.getSelectedRow() >= 0) {
                         if (JOptionPane.showConfirmDialog(StudentsFrame.this,
-                                "Вы хотите удалить студента?", "Удаление студента",
+                                "Р’С‹ С…РѕС‚РёС‚Рµ СѓРґР°Р»РёС‚СЊ СЃС‚СѓРґРµРЅС‚Р°?", "РЈРґР°Р»РµРЅРёРµ СЃС‚СѓРґРµРЅС‚Р°",
                                 JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-                            // Вот где нам пригодился метод getStudent(int rowIndex)
                             Student s = stm.getStudent(stdList.getSelectedRow());
                             try {
                                 ms.deleteStudent(s);
@@ -386,9 +323,9 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
                                 JOptionPane.showMessageDialog(StudentsFrame.this, e.getMessage());
                             }
                         }
-                    } // Если студент не выделен - сообщаем пользователю, что это необходимо
+                    }
                     else {
-                        JOptionPane.showMessageDialog(StudentsFrame.this, "Необходимо выделить студента в списке");
+                        JOptionPane.showMessageDialog(StudentsFrame.this, "РќРµРѕР±С…РѕРґРёРјРѕ РІС‹РґРµР»РёС‚СЊ СЃС‚СѓРґРµРЅС‚Р° РІ СЃРїРёСЃРєРµ");
                     }
                 }
             }
@@ -396,15 +333,13 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
         t.start();
     }
 
-    // метод для показа всех студентов
+    // РјРµС‚РѕРґ РґР»СЏ РїРѕРєР°Р·Р° РІСЃРµС… СЃС‚СѓРґРµРЅС‚РѕРІ
     private void showAllStudents() {
         //JOptionPane.showMessageDialog(this, "showAllStudents");
     	//----------------------------
     	Collection<Student> s;
 		try {
-			//Получаем список все студентов
 			s = ms.getAllStudents();
-        // Устанавливаем модель для таблицы с новыми данными
         stdList.setModel(new StudentTableModel(new Vector<Student>(s)));
         } catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -417,8 +352,8 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
 
             public void run() {
                 try {
-                    // Мы сразу отменим продолжение работы, если не сможем получить
-                    // коннект к базе данных
+                    // ГЊГ» Г±Г°Г Г§Гі Г®ГІГ¬ГҐГ­ГЁГ¬ ГЇГ°Г®Г¤Г®Г«Г¦ГҐГ­ГЁГҐ Г°Г ГЎГ®ГІГ», ГҐГ±Г«ГЁ Г­ГҐ Г±Г¬Г®Г¦ГҐГ¬ ГЇГ®Г«ГіГ·ГЁГІГј
+                    // ГЄГ®Г­Г­ГҐГЄГІ ГЄ ГЎГ Г§ГҐ Г¤Г Г­Г­Г»Гµ
                     StudentsFrame sf = new StudentsFrame();
                     sf.setDefaultCloseOperation(EXIT_ON_CLOSE);
                     sf.setVisible(true);
@@ -432,7 +367,7 @@ public class StudentsFrame extends JFrame implements ActionListener, ListSelecti
     }
 }
 
-// Наш внутренний класс - переопределенная панель.
+// РїРµСЂРµРѕРїСЂРµРґРµР»РµРЅРЅР°СЏ РїР°РЅРµР»СЊ
 class GroupPanel extends JPanel {
 
     public Dimension getPreferredSize() {
