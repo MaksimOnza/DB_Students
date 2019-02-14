7b904b6bc2c18781d50440ca1641f111d1462318
MaksimOnza <Maximus83onza@gmail.com>
2019-02-14 19:08:32 +0200

    Update GroupDialog.java

--- web/students/frame/GroupDialog.java
+++ web/students/frame/GroupDialog.java
@@ -30,103 +30,70 @@ public class GroupDialog extends JDialog implements ActionListener {
     private boolean result = false;
 
     public GroupDialog(int year, List<Group> groups) {
-        // Установить заголовок
         setTitle("Перенос группы");
 
-        // Создаем сложный layout для нашего окна
+        // layout для нашего окна
         GridBagLayout gbl = new GridBagLayout();
         setLayout(gbl);
-        // Создаем переменную для установки правил размещения
         GridBagConstraints c = new GridBagConstraints();
-        // Сразу задаем отступ от границ для каждого элемента
         c.insets = new Insets(5, 5, 5, 5);
 
-        // Первый элемент - заголовок для поля выбора групп
+        // Первый элемент
         JLabel l = new JLabel("Новая группа:");
-        // После него можно будет еще помещать компоненты
         c.gridwidth = GridBagConstraints.RELATIVE;
-        // Не заполняем все пространство, отведенное компоненту
         c.fill = GridBagConstraints.NONE;
-        // "Привязываем" компонент к правому краю
         c.anchor = GridBagConstraints.EAST;
-        // Устанавливаем это правило для нашего компонета
         gbl.setConstraints(l, c);
-        // Добавляем компонент
         getContentPane().add(l);
 
-        // Второй элемент - список групп
+        // Второй элемент
         groupList = new JComboBox(new Vector<Group>(groups));
-        // Элемент занимает всю оставшуюся ширину
         c.gridwidth = GridBagConstraints.REMAINDER;
-        // Растягиваем компонент по всему пространству для него
         c.fill = GridBagConstraints.BOTH;
-        // "Привязываем" его к левой части
         c.anchor = GridBagConstraints.WEST;
-        // Устанавливаем это правило для нашего компонета
         gbl.setConstraints(groupList, c);
-        // Добавляем компонент
         getContentPane().add(groupList);
 
-        // Третий элемент - заголовок для поля выбора года
+        // Третий элемент
         l = new JLabel("Новый год:");
-        // После него можно будет еще помещать компоненты
         c.gridwidth = GridBagConstraints.RELATIVE;
-        // Не заполняем все пространство, отведенное компоненту
         c.fill = GridBagConstraints.NONE;
-        // "Привязываем" компонент к правому краю
         c.anchor = GridBagConstraints.EAST;
-        // Устанавливаем это правило для нашего компонета
         gbl.setConstraints(l, c);
-        // Добавляем компонент
         getContentPane().add(l);
 
-        // Сразу увеличиваем группу на один год - для перевода
+        // увеличиваем группу на один год - для перевода
         spYear = new JSpinner(new SpinnerNumberModel(year + 1, 1900, 2100, 1));
-        // Элемент занимает всю оставшуюся ширину
         c.gridwidth = GridBagConstraints.REMAINDER;
-        // Растягиваем компонент по всему пространству для него
         c.fill = GridBagConstraints.BOTH;
-        // "Привязываем" его к левой части
         c.anchor = GridBagConstraints.WEST;
-        // Устанавливаем это правило для нашего компонета
         gbl.setConstraints(spYear, c);
-        // Добавляем компонент
         getContentPane().add(spYear);
 
         c.gridwidth = GridBagConstraints.RELATIVE;
         c.fill = GridBagConstraints.BOTH;
         btnOk.setName("OK");
-        // Добавляем листенер для кнопки
         btnOk.addActionListener(this);
-        // Устанавливаем это правило для нашего компонета
         gbl.setConstraints(btnOk, c);
-        // Добавляем компонент
         getContentPane().add(btnOk);
 
         btnCancel.setName("Cancel");
-        // Добавляем листенер для кнопки
         btnCancel.addActionListener(this);
-        // Устанавливаем это правило для нашего компонета
         gbl.setConstraints(btnCancel, c);
-        // Добавляем компонент
         getContentPane().add(btnCancel);
 
-        // Устанавливаем поведение формы при закрытии - не закрывать совсем.
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 
-        // Получаем размеры экрана
+        // Получаем размеры экрана и вычисляя координаты на основе полученной информации помещаем его по центру
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-        // А теперь просто помещаем его по центру, вычисляя координаты на основе полученной информации
         setBounds(((int) d.getWidth() - GroupDialog.D_WIDTH) / 2, ((int) d.getHeight() - GroupDialog.D_HEIGHT) / 2,
                 GroupDialog.D_WIDTH, GroupDialog.D_HEIGHT);
     }
 
-    // Возврат года, который установлен на форме
     public int getYear() {
         return ((SpinnerNumberModel) spYear.getModel()).getNumber().intValue();
     }
 
-    // Возврат группы, которая установлена на форме
     public Group getGroup() {
         if (groupList.getModel().getSize() > 0) {
             return (Group) groupList.getSelectedItem();
@@ -134,7 +101,6 @@ public class GroupDialog extends JDialog implements ActionListener {
         return null;
     }
 
-    // Получить результат, true - кнопка ОК, false - кнопка Cancel
     public boolean getResult() {
         return result;
     }
